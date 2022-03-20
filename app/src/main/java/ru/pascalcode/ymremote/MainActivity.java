9b69c811996d87ccb60e2186129f27e8201b484c
Timofeev <vtimofeev@gmail.com>
2022-03-20 11:07:30 +0500

    pre alfa huyalfa

--- app/src/main/java/ru/pascalcode/ymremote/MainActivity.java
+++ app/src/main/java/ru/pascalcode/ymremote/MainActivity.java
@@ -1,15 +1,11 @@
 package ru.pascalcode.ymremote;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.loader.content.AsyncTaskLoader;
-
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -18,7 +14,6 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Locale;
 
 import ru.pascalcode.ymremote.databinding.ActivityMainBinding;
 
@@ -28,54 +23,36 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
 
-    private Button button;
+    private Button playPause;
 
-    private EditText editText;
+    private Button previous;
+
+    private Button next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-//        binding.playPause.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                binding.playPause.setText("sdfsdf");
-//                if (binding.editTextTextPersonName.getText().toString().trim().equals("")) {
-//                    Toast.makeText(MainActivity.this, R.string.suk, Toast.LENGTH_LONG).show();
-//                } else {
-//
-//                }
-//            }
-//        });
-
-
         textView = findViewById(R.id.logo);
-        button = findViewById(R.id.playPause);
-        editText = findViewById(R.id.editTextTextPersonName);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (editText.getText().toString().trim().equals("")) {
-                    Toast.makeText(MainActivity.this, R.string.suk, Toast.LENGTH_LONG).show();
-                } else {
-                    String command = editText.getText().toString().toLowerCase(Locale.ROOT);
-                    String url = "http://192.168.0.5:1620/yandexmusic/" + command;
-
-                    new CommandSender().execute(url);
-
-                }
-            }
+        playPause = findViewById(R.id.playPause);
+        previous = findViewById(R.id.previous);
+        next = findViewById(R.id.next);
+        playPause.setOnClickListener(view -> {
+            String url = "http://192.168.0.5:1620/yandexmusic/playpause";
+            new CommandSender().execute(url);
+        });
+        previous.setOnClickListener(view -> {
+            String url = "http://192.168.0.5:1620/yandexmusic/previous";
+            new CommandSender().execute(url);
+        });
+        next.setOnClickListener(view -> {
+            String url = "http://192.168.0.5:1620/yandexmusic/next";
+            new CommandSender().execute(url);
         });
 
-    }
-
-
-    public void sendCommand() {
-        return;
     }
 
     private class CommandSender extends AsyncTask<String, String, String> {
@@ -83,7 +60,6 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            textView.setText("щаща");
         }
 
         @Override
